13bfa1ec4aafa8c0c62e7d6707441ad5c3fe5cf3
Tim Teillor <timteillor@gmail.com>
2021-12-12 03:45:21 +0200

    feat: parse trafic light

--- src/ExcelReader.js
+++ src/ExcelReader.js
@@ -70,6 +70,42 @@ import { file } from '@babel/types';
 
 function ExcelReader() {
   const [data, setData] = React.useState([]);
+
+  // UploadFile
+  async function UploadFile(event) {
+    const file = event.target.files[0];
+    
+    NormalizeData(file);
+  }
+
+  function NormalizeData(data) {
+    const traficLights = [];
+
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+        /* Parse data */
+        const bstr = evt.target.result;
+        const wb = XLSX.read(bstr, {type:'binary'});
+
+        /* Get first worksheet */
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+
+        /* Convert array of arrays */
+        const data = XLSX.utils.sheet_to_json(ws, {header:1});
+        /* Update state */
+
+        data.forEach(el => {
+          if(el[1] && el[2]) {
+            traficLights.push({"name": el[1].toString(), "value": el[2]})
+          }
+        });
+    };
+    reader.readAsBinaryString(data);
+
+    setData(traficLights);
+  }
+
   const [cols, setCols] = React.useState([]);
 
   const [fileCounter, setFileCounter] = React.useState(1);
@@ -94,13 +130,13 @@ function ExcelReader() {
     console.log("kek");
   }
 
-  const addfile = () => {
-    setFileCounter(fileCounter + 1);
-    if (fileCounter == 2) {
-      isSubmitVisibleSet(false);
-    }
-    console.log(fileCounter);
-  };
+  // const addfile = () => {
+  //   setFileCounter(fileCounter + 1);
+  //   if (fileCounter == 2) {
+  //     isSubmitVisibleSet(false);
+  //   }
+  //   console.log(fileCounter);
+  // };
 
   const exportFile = () => {
     /* convert state to workbook */
@@ -116,11 +152,11 @@ function ExcelReader() {
         <Row>
           <Col span={2}>
               Светофоры
-              <input type="file" className="form-control" id="file" accept={SheetJSFT} onChange={addfile} />
+              <input type="file" className="form-control" id="file" accept={SheetJSFT} onChange={UploadFile} />
               <br />
               <br />
               Обрывные места
-              <input type="file" className="form-control" id="file" accept={SheetJSFT} onChange={addfile} />
+              <input type="file" className="form-control" id="file" accept={SheetJSFT} onChange={UploadFile} />
           </Col>
           {/* <Col span={2}>
               Обрывные места
